5ea90b7e3a40c815ea7d02ce4c809d8b08a4ef63
atadityas13 <at.adityas13@gmail.com>
2026-04-20 11:58:29 +0700

    feat: implement Database class as a PDO wrapper for MySQL connectivity

--- includes/Database.php
+++ includes/Database.php
@@ -19,7 +19,7 @@ class Database {
         
         // Set Options
         $options = [
-            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_PERSISTENT => false,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ];
